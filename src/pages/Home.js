070ccb3e00325aadf1006e0676417c7410ba8d96
bhupendrasinghbutola <bhuppiammubutola@gmail.com>
2022-06-05 11:28:56 +0530

    error handled in previous code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,84 +1,95 @@
-// import { render } from '@testing-library/react';
-import React,{ useState } from 'react'
+import React,{useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
-import { apiGet } from '../components/misc/config';
+import { apiGet } from '../misc/config'
 
-const Home =()=>{
-  const [input,setInput]= useState('');
-  const [results,setResults]= useState(null);
-  const [searchOption,setSearchOPtion]= useState('shows');
-  const isShowsSearch =searchOption ==="shows";
-  const onSearch= () =>{
+const Home = () => {
+ const[input,setInput]= useState('')
+ const[results,setResults]= useState(null)
+ const[searchOption,setSearchOption]= useState('shows')
+ const isShowsSearch = searchOption ==='shows';
+ const onSearch =() =>{
 
-    apiGet(`search/$(searchOption)?q=$(input)`).then(r =>r.json())
-    .then(result =>{
-      setResults(result);
-      // console.log(result);
-    // https://api.tvmaze.com/search/shows?q=girls
+  apiGet(`/search/${searchOption}?q=${input}`).then(result=>{
+    setResults(result);
+    // console.log(result);
+  });
+};
+
+
+const  onInputChange = (ev)=>{
+  setInput(ev.target.value);  
+
+}
+
+const onKeyDown =(ev) =>{
+if (ev.keyCode=== 13){
+  onSearch();
+}
+}
+const renderResults= () =>{
+  if(results && results.length === 0){
+    return <div>No results</div>
+
+  }
+  if(results && results.length >0){
+    return results[0].show ? results.map((item)=>
+    (<div key={item.show.id}>{item.show.name}</div>
+    )) :  results.map((item)=>
+    (<div key={item.person.id}>{item.person.name}</div>
+    ));
     
 
-    })
-   }
-
-const onInputChange = ev =>{
-  setInput(ev.target.value);
- }
- 
-const onKeyDown=(ev) =>{
-  if(ev.keyCode===13){
-   onSearch()
-  }
-// console.log(ev.keyCode);
-}
-const onRadioChange =( ev) =>{
- setSearchOPtion(ev.target.value);
-}
-
-console.log(searchOption);
-
-
-
-const renderResults =()=>{
-  if(results && results.length ===0){
-    return <div>No results </div>
-  }
-  if(results && results.length>0){
-    return  results[0].shows ? results.map(item =>(
-    <div key={item.show.id}>{item.show.name}</div> 
-    )): results.map(item =>(
-      <div key={item.person.id}>{item.person.name}</div>
-    ));
   }
   return null;
-
 }
+const onRadioChange =(ev)=>{
+  setSearchOption(ev.target.value);
+}
+console.log(searchOption);
 
+  return  <MainPageLayout> 
 
-  return (
-    <MainPageLayout>
-      <input type="text"
-       onChange={onInputChange} 
-       onKeyDown={onKeyDown} 
-       value={input} 
-        placeholder="search for something"/>
+<input type="text" 
+placeholder='Search for something'
+onChange={onInputChange} 
+value={input} 
+onKeyDown={onKeyDown}/>
+
 <div>
-<label htmlFor="shows-search">
-  Shows
-  <input id='shows-search' type="radio" value="shows" onChange={onRadioChange} checked={isShowsSearch}/>
-</label>
+ <label htmlFor="shows-search">
+   Shows
+   <input id='shows-search'
+    type="radio"
+     value="shows"
+      onChange={onRadioChange} 
+      checked={isShowsSearch} />
+ </label>
+ <label htmlFor="actors-search">   Actors
+  <input id='actors-search' 
+  type="radio" 
+  value="people" 
+  onChange={onRadioChange} 
+   checked={!isShowsSearch}/>
+ </label>
 
-<label htmlFor="actors-search">
-  Actors
-  <input id='actors-search' type="radio" value="people" onChange={onRadioChange} checked={!isShowsSearch}/>
-</label>
 
 
 </div>
 
-      <button type='button' onClick={onSearch}>Search</button>
-      {renderResults()}
-    </MainPageLayout>
-  )
+
+<button type='button' onClick={onSearch}>Search
+</button>
+{renderResults()}
+  </MainPageLayout>
 }
 
 export default Home
+
+
+
+
+
+
+
+
+
