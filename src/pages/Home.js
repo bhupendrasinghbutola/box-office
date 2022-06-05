dcdbde09dbe4f544537f929e4a0ee3c883adaa70
bhupendrasinghbutola <bhuppiammubutola@gmail.com>
2022-06-05 12:54:51 +0530

    adding cards for show and actors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,95 +1,92 @@
-import React,{useState} from 'react'
-import MainPageLayout from '../components/MainPageLayout'
-import { apiGet } from '../misc/config'
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
 
 const Home = () => {
- const[input,setInput]= useState('')
- const[results,setResults]= useState(null)
- const[searchOption,setSearchOption]= useState('shows')
- const isShowsSearch = searchOption ==='shows';
- const onSearch =() =>{
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
 
-  apiGet(`/search/${searchOption}?q=${input}`).then(result=>{
-    setResults(result);
-    // console.log(result);
-  });
+  const isShowsSearch = searchOption === 'shows';
+
+  const onSearch = () => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        placeholder="Search for something"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+
+        <label htmlFor="actors-search">
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
+
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
 };
 
-
-const  onInputChange = (ev)=>{
-  setInput(ev.target.value);  
-
-}
-
-const onKeyDown =(ev) =>{
-if (ev.keyCode=== 13){
-  onSearch();
-}
-}
-const renderResults= () =>{
-  if(results && results.length === 0){
-    return <div>No results</div>
-
-  }
-  if(results && results.length >0){
-    return results[0].show ? results.map((item)=>
-    (<div key={item.show.id}>{item.show.name}</div>
-    )) :  results.map((item)=>
-    (<div key={item.person.id}>{item.person.name}</div>
-    ));
-    
-
-  }
-  return null;
-}
-const onRadioChange =(ev)=>{
-  setSearchOption(ev.target.value);
-}
-console.log(searchOption);
-
-  return  <MainPageLayout> 
-
-<input type="text" 
-placeholder='Search for something'
-onChange={onInputChange} 
-value={input} 
-onKeyDown={onKeyDown}/>
-
-<div>
- <label htmlFor="shows-search">
-   Shows
-   <input id='shows-search'
-    type="radio"
-     value="shows"
-      onChange={onRadioChange} 
-      checked={isShowsSearch} />
- </label>
- <label htmlFor="actors-search">   Actors
-  <input id='actors-search' 
-  type="radio" 
-  value="people" 
-  onChange={onRadioChange} 
-   checked={!isShowsSearch}/>
- </label>
-
-
-
-</div>
-
-
-<button type='button' onClick={onSearch}>Search
-</button>
-{renderResults()}
-  </MainPageLayout>
-}
-
-export default Home
-
-
-
-
-
-
-
-
-
+export default Home;
