5bc45868a1a6adb82131bd5bf24f4df644723a87
bhupendrasinghbutola <bhuppiammubutola@gmail.com>
2022-06-07 12:29:10 +0530

    created show page components and populated them with data

--- src/misc/config.js
+++ src/misc/config.js
@@ -1,8 +1,16 @@
-const API_BASE_URL='https://api.tvmaze.com';
+// const API_BASE_URL='https://api.tvmaze.com';
 
- export async function apiGet(querryString){
-    const response = await fetch( `${API_BASE_URL}${querryString}`) .then(r =>r.json())
-    return response;
-};
+//  export async function apiGet(querryString){
+//     const response = await fetch( `${API_BASE_URL}${querryString}`) .then(r =>r.json())
+//     return response;
+// };
 
 // return response;
+const API_BASE_URL = 'https://api.tvmaze.com';
+
+export async function apiGet(queryString) {
+  const response = await fetch(`${API_BASE_URL}${queryString}`).then(r =>
+    r.json()
+  );
+  return response;
+}
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,64 +1,99 @@
-import React ,{useEffect,useReducer} from 'react'
-import { useParams} from 'react-router-dom'
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useReducer } from 'react';
+import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+import ShowMainData from '../components/show/ShowMainData';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import Cast from '../components/show/Cast';
 
-const reducer =(prevState,action)=>{
-  switch(action.type){
-    case 'FETCH_SUCCESS':{
-      return{isLoading:false,error:null,show:action.show}
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
     }
-    case 'FETCH_FAILED':{
-      return {...prevState,isLoading:false,error:action.error}
+
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
     }
-    default:return prevState;
+
+    default:
+      return prevState;
   }
+};
 
-}
-
-const InitialState={
-  show:null,
-  isLoading:true,
-  error:null
-}
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
 
 const Show = () => {
-  const {id} = useParams();
-   const [state,dispatch] =useReducer(reducer,InitialState)
-  // const [show,setShow] =useState(null);
-  // const[isLoading,setIsLoading]=useState(true);
-  // const[error,setError]= useState(null);
-//   console.log('params',params);
-console.log('state',state);
+  const { id } = useParams();
 
-  useEffect( ()=>{
-let isMounted=true;
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
-     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results=>{
-         
-           if(isMounted){ 
+  useEffect(() => {
+    let isMounted = true;
 
-dispatch({type:'FETCH_SUCCESS', show:results})}
-}).catch(err =>{
-       if(isMounted)
-        dispatch({type:'FETCH_FAILED',error:err.message})
-      
-         
-     })
-     return () =>{
-       isMounted=false;
-     }
-  }, [id] )
-  // console.log('show',show);
-  // if(isLoading){
-  //   return  <div> Data is being loaded </div>
-  // }
-  // if(error){
-  //   return  <div> Error occured:{error} </div>
-  // }
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+        }
+      });
 
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
 
-  return  <div> this is show page. </div>
-  
-}
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
 
-export default Show
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
+
+  return (
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
+};
+
+export default Show;
