aaa146aa925737e661a6b981381536f7e2485fd7
bhupendrasinghbutola <bhuppiammubutola@gmail.com>
2022-06-07 23:26:15 +0530

    using custom hook for starred shows

--- src/components/styled.js
+++ src/components/styled.js
@@ -1,44 +1,45 @@
 import styled from 'styled-components';
 
 export const FlexGrid = styled.div`
-display:flex;
-justify-content:center;
-flex-wrap:wrap;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
+
 export const SearchCard = styled.div`
-width:300px;
-height:100%;
-margin:0 15px 40px;
+  width: 300px;
+  height: 100%;
+  margin: 0 15px 40px;
 
-.img-wrapper{
-    // border-radius:55%;
-    height:420px;
-    width:100%;
-    overflow:hidden;
-    // border:1px solid #ddd;
-    
+  .img-wrapper {
+    width: 100%;
+    border-radius: 40px;
+    height: 420px;
+    overflow: hidden;
+    border: 1px solid #ddd;
 
-img{
-    object-fit:cover;
-    height:100%;
-    width:100%;
-}}
+    img {
+      object-fit: cover;
+      height: 100%;
+      width: 100%;
+    }
+  }
 
+  h1 {
+    margin: 10px 0;
+    font-size: 21px;
+  }
 
-h1{
-    margin:10px 0;
-    font-size:21px;
-}
-p{
-    margin:0;
-}
-
+  p {
+    margin: 0;
+  }
 `;
+
 export const Star = styled.div`
   display: inline-block;
   width: 18px;
   height: 18px;
-  background-color: #ffc806;
+  background-color: ${props => (props.active ? '#ffc806' : '#ddd')};
   clip-path: polygon(
     50% 0%,
     61% 35%,
@@ -52,9 +53,3 @@ export const Star = styled.div`
     39% 35%
   );
 `;
-
-
-
-
-
-
