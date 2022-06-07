e054a2d3d62fdabda190291ae6861396aa0392dc
bhupendrasinghbutola <bhuppiammubutola@gmail.com>
2022-06-08 00:54:27 +0530

    created 2 more hooks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,10 @@ import MainPageLayout from '../components/MainPageLayout';
 import { apiGet } from '../misc/config';
 import ShowGrid from '../components/show/ShowGrid';
 import ActorGrid from '../components/actor/ActorGrid';
+import { useLastQuerry } from '../misc/custom-hooks';
 
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuerry();
   const [results, setResults] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
 
